d0ba74322a1c0162aefbd8efc29c6d31a265de49
Mário Nascimento <mario@whitehathacking.tech>
2023-05-26 12:00:49 +0100

    update game list and user list

--- src/components/ProfileDetail.vue
+++ src/components/ProfileDetail.vue
@@ -4,8 +4,8 @@
             <div class="col-12 col-md-8 col-lg-6">
                 <div class="card">
                     <div class="card-body text-center">
-                        <img src="../assets/avatar.png" alt="Profile Image" class="img-fluid rounded-circle mb-3"
-                            style="width: 150px; height: 150px; object-fit: cover;">
+                        <img :src="profileImageUrl" alt="Profile Image" class="img-fluid rounded-circle mb-3"
+                            style="width: 150px; height: 150px; object-fit: cover;" />
                         <h1 class="h4 mb-0">{{ user.name }}</h1>
                         <p class="text-muted">{{ user.email }}</p>
                         <div class="btn-group" role="group" aria-label="Profile Actions">
@@ -23,15 +23,15 @@
                             <form @submit.prevent="saveProfile()">
                                 <div class="form-group">
                                     <label for="name"><strong>Name:</strong></label>
-                                    <input type="text" id="name" v-model="user.name" class="form-control">
+                                    <input type="text" id="name" v-model="user.name" class="form-control" />
                                 </div>
                                 <div class="form-group">
                                     <label for="email"><strong>Email:</strong></label>
-                                    <input type="email" id="email" v-model="user.email" class="form-control">
+                                    <input type="email" id="email" v-model="user.email" class="form-control" />
                                 </div>
                                 <div class="form-group">
                                     <label for="profile-image">Profile Image:</label>
-                                    <input type="file" id="profile-image" @change="handleProfileImageChange">
+                                    <input type="file" id="profile-image" @change="handleProfileImageChange" />
                                 </div>
                                 <button class="btn btn-primary">Save</button>
                             </form>
@@ -42,22 +42,21 @@
                                 <div class="form-group">
                                     <label for="current-password"><strong>Current Password:</strong></label>
                                     <input type="password" id="current-password" v-model="passwords.currentPassword"
-                                        class="form-control">
+                                        class="form-control" />
                                 </div>
                                 <div class="form-group">
                                     <label for="new-password"><strong>New Password:</strong></label>
                                     <input type="password" id="new-password" v-model="passwords.newPassword"
-                                        class="form-control">
+                                        class="form-control" />
                                 </div>
                                 <div class="form-group">
                                     <label for="confirm-password"><strong>Confirm Password:</strong></label>
                                     <input type="password" id="confirm-password" v-model="passwords.confirmPassword"
-                                        class="form-control">
+                                        class="form-control" />
                                 </div>
                                 <button class="btn btn-primary">Save</button>
                             </form>
                         </div>
-
 
                         <div v-else>
                             <div class="form-group">
@@ -71,14 +70,26 @@
                         </div>
                     </div>
                 </div>
+
+                <div class="card mt-3">
+                    <div class="card-body">
+                        <h3>My Games</h3>
+                        <ul>
+                            <li v-for="game in user.games" :key="game.game_id" class="game-item">
+                                {{ game.game_name }}
+                                <button class="btn btn-danger" @click="removeGameFromUser(game)">Remove</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
-
-
+  
 <script>
 import Cookies from 'js-cookie';
+
 export default {
     data() {
         return {
@@ -87,14 +98,15 @@ export default {
             user: {
                 name: '',
                 email: '',
-                profileImageUrl: '../assets/avatar.png'
+                profileImageUrl: '../assets/avatar.png',
+                games: [],
             },
             profileImageFile: null,
             passwords: {
                 currentPassword: '',
                 newPassword: '',
-                confirmPassword: ''
-            }
+                confirmPassword: '',
+            },
         };
     },
     async created() {
@@ -103,25 +115,53 @@ export default {
     computed: {
         profileImageUrl() {
             return this.profileImageFile ? URL.createObjectURL(this.profileImageFile) : this.user.profileImageUrl;
-        }
+        },
     },
     methods: {
         async fetchUserData() {
             try {
-                const token = Cookies.get('token'); // Get the bearer token from cookies
-                const resp = await fetch("http://127.0.0.1:7000/api/users/me", {
+                const token = Cookies.get('token');
+                const resp = await fetch('http://127.0.0.1:7000/api/users/me', {
                     headers: {
-                        Authorization: `Bearer ${token}`
-                    }
+                        Authorization: `Bearer ${token}`,
+                    },
                 });
 
                 const data = await resp.json();
 
-                this.user.name = data.first_name + " " + data.last_name;
+                this.user.name = data.first_name + ' ' + data.last_name;
                 this.user.email = data.email;
                 this.user.profileImageUrl = data.profileImageUrl;
+                this.user.games = data.games;
             } catch (error) {
-                console.error("Error fetching user data:", error);
+                console.error('Error fetching user data:', error);
+            }
+        },
+
+        async removeGameFromUser(game) {
+            try {
+                const token = Cookies.get('token');
+                const response = await fetch(`http://127.0.0.1:7000/api/users/games/${game.game_id}`, {
+                    method: 'DELETE',
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
+                if (response.ok) {
+                    console.log(`Game ${game.game_name} removed from user`);
+                    // Remove the game from the user's games array
+                    const index = this.user.games.findIndex((g) => g.game_id === game.game_id);
+                    if (index !== -1) {
+                        this.user.games.splice(index, 1);
+                    }
+                } else {
+                    throw new Error('Failed to remove game from user');
+                    // Handle error
+                }
+            } catch (err) {
+                console.error(err);
+                // Handle error
             }
         },
 
@@ -143,18 +183,15 @@ export default {
         },
         logout() {
             this.$store.dispatch('logout');
-        }
-    }
+        },
+    },
 };
 </script>
-
-
-
-
+  
 <style scoped>
 .card {
     background-color: #333333;
-    color: #FFFFFF;
+    color: #ffffff;
     border-radius: 10px;
     box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
     margin: 20px;
@@ -163,7 +200,7 @@ export default {
 
 .card-header {
     background-color: #8bc34a;
-    color: #FFFFFF;
+    color: #ffffff;
     padding: 20px;
 }
 
@@ -188,9 +225,9 @@ export default {
     font-size: 18px;
 }
 
-.form-group input[type="text"],
-.form-group input[type="email"] {
-    background-color: #FFFFFF;
+.form-group input[type='text'],
+.form-group input[type='email'] {
+    background-color: #ffffff;
     border-radius: 3px;
     border: none;
     display: block;
@@ -213,4 +250,12 @@ export default {
     font-size: 18px;
     padding-top: 5px;
 }
+
+.game-item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 10px;
+}
 </style>
+  
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,41 +1,147 @@
 <template>
     <div class="games">
         <h1>{{ $t("games.title") }}</h1>
-        <div class="game-list-container">
-            <div class="game-list">
-                <div v-for="game in orderedGames" :key="game.id" class="game-item d-flex align-items-center mb-3">
-                    <div class="game-name">{{ game.game_name }}&nbsp;&nbsp;</div>
-                    <div class="icon-container">
-                        <i :class="game.selected ? 'fa fa-minus-circle' : 'fa fa-plus-circle'"
-                            @click="toggleGameSelection(game)"></i>
+        <div class="game-container">
+            <div class="game-list-container">
+                <h2>Remaining Games</h2>
+                <input type="text" v-model="remainingSearch" placeholder="Search for games" class="search-bar" />
+                <div class="game-list">
+                    <div v-for="game in filteredRemainingGames" :key="game.game_id"
+                        class="game-item d-flex align-items-center mb-3">
+                        <div class="game-name">{{ game.game_name }}</div>
+                        <div class="icon-container">
+                            <i class="fa fa-plus-circle" @click="addGameToUser(game)"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="game-list-container">
+                <h2>User's Games</h2>
+                <input type="text" v-model="userSearch" placeholder="Search for games" class="search-bar" />
+                <div class="game-list">
+                    <div v-for="game in filteredUserGames" :key="game.game_id"
+                        class="game-item d-flex align-items-center mb-3">
+                        <div class="game-name">{{ game.game_name }}</div>
+                        <div class="icon-container">
+                            <i class="fa fa-minus-circle" @click="removeGameFromUser(game)"></i>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
-
   
 <script>
+import Cookies from 'js-cookie';
+
 export default {
     name: 'Games',
     data() {
         return {
             games: [],
+            user: {
+                games: [],
+            },
+            remainingSearch: '',
+            userSearch: '',
         };
     },
     computed: {
-        orderedGames() {
-            return this.games.sort((a, b) => b.selected - a.selected);
+        filteredRemainingGames() {
+            const remainingGames = this.remainingGames;
+            const searchQuery = this.remainingSearch.toLowerCase();
+            if (!searchQuery) {
+                return remainingGames;
+            }
+            return remainingGames.filter(game => game.game_name.toLowerCase().includes(searchQuery));
+        },
+        filteredUserGames() {
+            const userGames = this.user.games;
+            const searchQuery = this.userSearch.toLowerCase();
+            if (!searchQuery) {
+                return userGames;
+            }
+            return userGames.filter(game => game.game_name.toLowerCase().includes(searchQuery));
+        },
+        remainingGames() {
+            const userGameIds = this.user.games.map(game => game.game_id);
+            return this.games.filter(game => !userGameIds.includes(game.game_id));
         },
     },
     methods: {
-        toggleGameSelection(game) {
-            game.selected = !game.selected;
-            if (game.selected) {
-                console.log(`Adding game: ${game.game_name}`);
-            } else {
-                console.log(`Removing game: ${game.game_name}`);
+        async fetchUserData() {
+            try {
+                const token = Cookies.get('token');
+                const resp = await fetch('http://127.0.0.1:7000/api/users/games', {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
+                if (resp.ok) {
+                    const data = await resp.json();
+                    console.log(data);
+                    this.user.games = data; // Update the user's games array
+                } else {
+                    throw new Error('Failed to fetch user data');
+                    // Handle error
+                }
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+                // Handle error
+            }
+        },
+        async addGameToUser(game) {
+            try {
+                const token = Cookies.get('token');
+                const response = await fetch('http://127.0.0.1:7000/api/users/games', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${token}`,
+                    },
+                    body: JSON.stringify({
+                        game_id: game.game_id,
+                    }),
+                });
+
+                if (response.ok) {
+                    console.log(`Game ${game.game_name} added to user`);
+                    this.user.games.push(game); // Add the game to the user's games array
+                } else {
+                    throw new Error('Failed to add game to user');
+                    // Handle error
+                }
+            } catch (err) {
+                console.error(err);
+                // Handle error
+            }
+        },
+        async removeGameFromUser(game) {
+            try {
+                const token = Cookies.get('token');
+                const response = await fetch(`http://127.0.0.1:7000/api/users/games/${game.game_id}`, {
+                    method: 'DELETE',
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
+                if (response.ok) {
+                    console.log(`Game ${game.game_name} removed from user`);
+                    // Remove the game from the user's games array
+                    const gameIndex = this.user.games.findIndex(g => g.game_id === game.game_id);
+                    if (gameIndex > -1) {
+                        this.user.games.splice(gameIndex, 1);
+                    }
+                } else {
+                    throw new Error('Failed to remove game from user');
+                    // Handle error
+                }
+            } catch (err) {
+                console.error(err);
+                // Handle error
             }
         },
     },
@@ -45,13 +151,15 @@ export default {
             const games = await response.json();
             console.log(games);
             this.games = games.map(game => ({ ...game, selected: false }));
+
+            await this.fetchUserData();
         } catch (error) {
             console.error('Error fetching games:', error);
         }
     },
 };
 </script>
-
+  
 <style scoped>
 .games {
     display: flex;
@@ -66,10 +174,16 @@ h1 {
     margin-bottom: 20px;
 }
 
+.game-container {
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+}
+
 .game-list-container {
-    width: 60%;
+    flex: 1;
     max-height: 400px;
-    /* padding: 20px; */
+    padding: 20px;
     border: 1px solid #ddd;
     border-radius: 5px;
     background-color: #f8f9fa;
@@ -77,6 +191,11 @@ h1 {
     overflow-y: auto;
     margin-bottom: 20px;
     color: #000;
+}
+
+h2 {
+    margin: 10px 0;
+    text-align: center;
 }
 
 .game-list {
@@ -88,8 +207,31 @@ h1 {
 .game-item {
     width: 100%;
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
+    align-items: center;
     font-size: 24px;
 }
-</style>
 
+.icon-container {
+    cursor: pointer;
+}
+
+.fa-plus-circle {
+    color: green;
+}
+
+.fa-minus-circle {
+    color: red;
+}
+
+.search-bar {
+    margin-bottom: 10px;
+    padding: 8px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    font-size: 14px;
+    width: 100%;
+    box-sizing: border-box;
+}
+</style>
+  
